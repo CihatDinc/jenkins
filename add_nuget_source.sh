c60d585088450f4177b8c909ea64911cfe10eac4
CihatDinc <cihatdinc@msn.com>
2023-09-29 15:34:57 +0300

    update

--- add_nuget_source.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dotnet nuget add source \
-    --username $GITHUB_USERNAME \
-    --password $GITHUB_ACCESS_TOKEN \
-    --store-password-in-clear-text \
-    --name github $GITHUB_PACKAGE_URL
